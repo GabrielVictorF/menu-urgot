7060e19a397ec2636f2a0db80527c6af6025b520
Gabriel <gabriel.victor.ti@gmail.com>
2018-03-07 20:35:49 -0300

    Layout das páginas lanches e bebidas criados

--- src/pages/bebida/bebida.ts
+++ src/pages/bebida/bebida.ts
@@ -10,7 +10,7 @@ export class BebidaPage {
 
   }
 
-  public bebidas = [
+  public bebidas_refrigerante = [
   	{
   		id: 0,
   		img: "bebida-coca-cola.jpg",
@@ -28,6 +28,48 @@ export class BebidaPage {
   		img: "bebida-ice-cola.jpeg",
   		nome: "Ice cola",
   		preco: 4.50
+  	}
+  ]
+
+  public bebidas_cha = [
+	{
+		id: 0,
+		img: "cha-preto.jpeg",
+  		nome: "Chá Preto",
+  		preco: 23.50
+	},
+	{
+		id: 1,
+		img: "cha-hortela.jpeg",
+  		nome: "Chá de Hortelã",
+  		preco: 13.50
+	},
+	{
+		id: 1,
+		img: "cha-canela.jpeg",
+  		nome: "Chá de Canela",
+  		preco: 6.80
+	}
+  ]
+
+  public bebidas_suco = [
+  	{
+  		id: 0,
+		img: "suco-melancia.jpeg",
+  		nome: "Melancia",
+  		preco: 23.50
+  	},
+  	{
+  		id: 1,
+		img: "suco-tangerina.jpeg",
+  		nome: "Tangerina",
+  		preco: 23.50
+  	},
+  	{
+  		id: 2,
+		img: "suco-limao.jpeg",
+  		nome: "Limão",
+  		preco: 23.50
   	}
   ]
 }
--- src/pages/bolo/bolo.ts
+++ src/pages/bolo/bolo.ts
@@ -11,4 +11,45 @@ export class BoloPage {
 
   }
 
+  public sanduiches = [
+  	{
+  		id: 0,
+  		img: "sanduiche-x-salada.jpeg",
+  		nome: "X-salada",
+  		preco: 3
+  	},
+  	{
+  		id: 1,
+  		img: "sanduiche-misto.jpeg",
+  		nome: "Misto",
+  		preco: 3
+  	},
+  	{
+  		id: 2,
+  		img: "sanduiche-hamburguer.jpeg",
+  		nome: "Hambúrguer",
+  		preco: 3
+  	}
+  ]
+
+  public pasteis = [
+  	{
+  		id: 0,
+  		img: "pastel-carne.jpeg",
+  		nome: "Carne",
+  		preco: 6
+  	},
+  	{
+  		id: 1,
+  		img: "pastel-frango.jpeg",
+  		nome: "Frango",
+  		preco: 3
+  	},
+  	{
+  		id: 2,
+  		img: "pastel-calabresa.jpeg",
+  		nome: "Calabresa",
+  		preco: 3
+  	}
+  ]
 }
