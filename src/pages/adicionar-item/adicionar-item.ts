1f0d1e9d00f6218749ce6ba928708150e28a890c
Gabriel <gabriel.victor.ti@gmail.com>
2018-04-06 21:52:56 -0300

    Refeita engenharia de compra

--- src/pages/adicionar-item/adicionar-item.ts
+++ src/pages/adicionar-item/adicionar-item.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the AdicionarItemPage page.
@@ -15,12 +15,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AdicionarItemPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
   }
 
   private Item = this.navParams.get('Item');
+  public carrinho = new Array<any>();
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad AdicionarItemPage');
   }
 
+  compra(version) {
+  	let prompt = this.alertCtrl.create({
+      title: this.Item.nome,
+      message: "Quantas Unidades?",
+      inputs: [
+        {
+          name: 'qtd',
+          placeholder: 'Vou querer...',
+          type: 'number'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+          	this.carrinho.push(this.Item);
+            console.log(this.carrinho);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
